48ff387ce6334fb216b67f5ee2373a2a98ce2f72
SantMu123 <smunozcastaneda14@gmail.com>
2024-08-12 21:49:00 -0500

    feat: :sparkles: Comunicacion con elemento padre y validacion

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,19 +1,23 @@
-import React from "react"
 import  {useState} from "react"
 import { AddCategory } from "./components/AddCategory";
 
 export const GifExportApp = () => {
     const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
-    const HandleAddCategory = () => {
-        setCategories([...categories, "Hola"])
+    const HandleAddCategory = ( newCategory ) => {
+        if(categories.includes(newCategory)) return;
+        
+        setCategories([newCategory, ...categories])
     }
 
     return (
         <>
             <h1>GifExpertApp</h1>
 
-            <AddCategory setCategories = {setCategories}/>
+            <AddCategory 
+                //setCategories = {setCategories}
+                onNewCategory = {event => HandleAddCategory(event)}
+            />
             <button onClick = {HandleAddCategory}>Añadir</button>
             <ol>
                 {categories.map(category => {
